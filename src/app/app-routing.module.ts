4e8eaab2c00ad1c1188a2e28407727250e044f0f
K M Rifat ul alom <kmrifat@gmail.com>
2023-01-14 03:46:45 +0600

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PopoverComponent} from "./page/popover/popover.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PopoverComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PopoverComponent } from './page/popover/popover.component';
+import { PopoverDirective } from './directives/popover.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopoverComponent,
+    PopoverDirective,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
